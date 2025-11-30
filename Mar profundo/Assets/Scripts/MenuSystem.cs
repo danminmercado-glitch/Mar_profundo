619ae5bae66ef97336373edf6a1a16cd487729fe
Braian Garcia Acosta <braian.garcia@uniat.edu.mx>
2025-11-30 15:14:44 -0600

    Menu
    Menu menos las opciones

---
+++ Mar profundo/Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void Opciones()
+    {
+        
+    }
+}
