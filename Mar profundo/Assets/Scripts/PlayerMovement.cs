e8f4556c779194b2a60135fd828f48869f931409
Braian Garcia Acosta <braian.garcia@uniat.edu.mx>
2026-01-19 11:14:43 -0600

    Movimiento actualizado

--- Mar profundo/Assets/Scripts/PlayerMovement.cs
+++ Mar profundo/Assets/Scripts/PlayerMovement.cs
@@ -1,42 +1,39 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-//Codigo para el movimiento de el jugador usando el sitema de Input de Unity
+
 public class PlayerMovement : MonoBehaviour
 {
-    //Declarar variables
     public Rigidbody rb;
-    [SerializeField] private float upForce = 265f, force = 10f;
+    [SerializeField] private float moveForce = 10f;
+    [SerializeField] private float jumpForce = 265f;
+
     private PlayerInput playerInput;
     private Vector2 input;
 
-    //Toma el rigidbody en una variable para un facil uso y el componente de Input
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
+
+        rb.freezeRotation = true; // Evita que el personaje se voltee
     }
 
-    // Update is called once per frame
-    //Ajusta el input con los pulsados de botones
     private void Update()
     {
         input = playerInput.actions["Move"].ReadValue<Vector2>();
-
     }
 
-    //Fuerzas de movimiento horizontal en FixedUpdate ya que maneja mejores las fisicas
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector2(input.x, 0f) * force);
+        Vector3 movement = new Vector3(input.x, 0f, input.y);
+        rb.AddForce(movement * moveForce, ForceMode.Force);
     }
 
-    //Sincronizar el imput de jump con el script y hace que solo salte en la face performed cuando se presiona el boton
-    public void Jump(InputAction.CallbackContext callbackContext)
+    public void Jump(InputAction.CallbackContext context)
     {
-        if(callbackContext.performed)
+        if (context.performed)
         {
-            rb.AddForce(Vector2.up * upForce);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
-
 }
